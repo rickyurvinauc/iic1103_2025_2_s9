22990e23acad29f159926a60d7a67786de8844b5
ricardourvina <rurvinac@estudiante.uc.cl>
2025-11-04 12:57:18 -0300

    OOP

--- OOPII/P1_laboratorioA.py
+++ OOPII/P1_laboratorioA.py
@@ -1,16 +1,20 @@
+# Escribe tu código aquí
+
 class Muestra:
+
     def __init__(self, tipo, peso, fragil):
         self.tipo = tipo
         self.peso = peso
         self.fragil = fragil
 
+    def __str__(self):
+        texto = "Muestra de tipo "+ self.tipo +" y peso "+ str(self.peso)+"g"
+        return texto
+
     def riesgo(self):
-        if self.tipo in ["sangre", "orina"] and self.peso > 100:
-            return "ALTO"
-        elif self.fragil and self.tipo not in ["sangre", "orina"]:
-            return "ALTO"
+        if ((self.tipo == "sangre" or self.tipo == "orina") and self.peso > 100 ) \
+         or (self.fragil == True and self.tipo != "sangre" and self.tipo != "orina"):
+         return "ALTO"
         else:
             return "BAJO"
-
-    def __str__(self):
-        return f"Muestra de tipo {self.tipo} y peso {self.peso}g"
+        
--- OOPII/clase.py
+++ OOPII/clase.py
@@ -1,7 +1,38 @@
-class Persona:
-    def saludar(self):
-        print("Hola, soy una persona")
+class Estudiante:
 
-p1 = Persona()
-Persona.saludar(p1)
-p1.saludar()
+    def __init__(self, identificador ,nombre, carrera):
+        self.identificador = nombre
+        self.nombre = nombre
+        self.carrera = carrera
+        self.notas = [] # [5,4,6]
+
+    def __str__(self):
+        promedio = 0
+        if len(self.notas) > 0:
+            promedio = sum(self.notas) /len(self.notas)
+        texto = "Hola soy: "+self.nombre+ " y mi promedio de notas es: "+ str(promedio)
+        return texto
+
+    def agregar_nota(self,nota):
+        self.notas.append(nota) # modificado un atributo del objeto
+
+    def notas_mayor_que_4(self):
+        if sum(self.notas) / len(self.notas) > 4:
+            return True
+        return False
+    
+    def cambiar_nombre(self, nombre):
+        self.nombre = nombre
+
+    def misma_carrea(self, estudiante2):
+        if self.carrera == estudiante2.carrera:
+            return True
+        return False
+
+vivi = Estudiante(1,"Vivi","Ing. Civil")
+ricardo = Estudiante(2, "Ricardo","Dc. DCC")
+
+print(vivi) # Hola soy Vivi y mi promedio de notas es 4.5
+
+nombre = "Ricardo"
+print(type(vivi))

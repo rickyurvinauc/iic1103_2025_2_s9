c5f18a1fb41f38a02d197b4dd84b383c3f40ffb3
ricardourvina <rurvinac@estudiante.uc.cl>
2025-11-05 23:37:07 -0300

    repaso i2

--- interaccion_objetos/P2_laboratorioB.py
+++ interaccion_objetos/P2_laboratorioB.py
@@ -1,29 +1,25 @@
 class Caja:
-    def __init__(self, peso_max):
+    def __init__(self, peso_max, muestras=[]):
         self.peso_max = peso_max
-        self.muestras = []
+        self.muestras = muestras
 
-    def agregar_muestra(self, nueva):
-        total_actual = 0
-        for m in self.muestras:
-            total_actual += m.peso
-
-        if total_actual + nueva.peso <= self.peso_max:
-            if self.muestras != []:
-                if self.muestras[0].riesgo() == nueva.riesgo():
-                    self.muestras.append(nueva)
-                    return True
-                else:
-                    return False
-            else:
-                self.muestras.append(nueva)
-                return True
-        else:
-            return False
+    def agregar_muestra(self, muestra):
+        if len(self.muestras) == 0 and self.peso_max <= muestra.peso:
+            self.muestras.append(muestra)
+            return True
+        peso_actual = 0
+        for mues in self.muestras:
+            peso_actual += mues.peso
+        peso_total = peso_actual + muestra.peso
+        tipo_pri = self.muestras[0].riesgo()
+        if peso_total <= self.peso_max and tipo_pri == muestra.riesgo():
+            self.muestras.append(muestra)
+            return True
+        return False
 
     def contar_fragiles(self):
-        n = 0
-        for m in self.muestras:
-            if m.fragil:
-                n += 1
-        return n
+        conteo = 0
+        for muestra in self.muestras:
+            if muestra.fragil == True:
+                conteo += 1
+        return conteo
